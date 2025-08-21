3ce4513581a021989246f8090cf7b0b064f1122e
Hamim <hamim.softvence@gmail.com>
2025-08-22 02:30:03 +0600

    update daynamic Navbar

--- src/app/(app)/settings/page.tsx
+++ src/app/(app)/settings/page.tsx
@@ -222,7 +222,7 @@ export default function SettingsPage() {
                       placeholder="Enter current password"
                       value={oldPassword}
                       onChange={(e) => setOldPassword(e.target.value)}
-                      className="w-full h-10 px-3 py-2.5 bg-slate-50 rounded-md border border-slate-200 placeholder-slate-500 font-Nunito pr-10"
+                      className="w-full text-gray-700 h-10 px-3 py-2.5 bg-slate-50 rounded-md border border-slate-200 placeholder-slate-500 font-Nunito pr-10"
                       required
                     />
                     <button
